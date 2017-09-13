4cd12ad3a9d0ea467ba6a607b4b2a9199e854c48
vivien208 <1160314836>
2017-09-13 11:28:37 +0800

    update code
    update code

---
+++ vuestart/src/component/firstcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="firstcomponent">
+      <h1>I am title</h1>
+      <a>written by {{author}} </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      author: "guo xiaowei"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
